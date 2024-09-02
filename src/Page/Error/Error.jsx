3f28262328d87215ca2ba88cefb60a0776048729
Shahriyar <chyshahriyar@gmail.com>
2024-09-02 22:21:54 +0600

    Error page completed

--- src/Page/Error/Error.jsx
+++ src/Page/Error/Error.jsx
@@ -1,10 +1,18 @@
-
+import { Link } from 'react-router-dom';
+import error from '../../../public/Error.jpg'
 
 const Error = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="text-center mt-7">
+                <h1 className="text-5xl uppercase font-semibold">Sorry</h1>
+                <p className="text-2xl text-[#373636b9]">This page is not found.</p>
+            </div>
+            <img src={error} alt="Error image" className='w-6/12 mx-auto h-3/6 my-3' />
+            <div className='text-center'>
+                <Link to='/' className='btn bg-[#F7A582] text-[#FFFFFF] text-xl px-12 py-3 rounded-xl'>Back to Home</Link>
+            </div>
+        </>
     );
 };
 
